1b2bd9d4618789e8a8964e3091d1beaf7e406d25
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2016-03-29 22:23:51 +0200

    was able to drastically simplify imports for examples because of install script

--- examples/convolution.py
+++ examples/convolution.py
@@ -1,40 +1,30 @@
 #!/usr/bin/env python
-
 import numpy
+import kernel_tuner
 
-if __name__ == '__main__':
-    #this boilerplate is to enable relative imports when executing a script
-    if  __package__ is None:
-        import sys
-        from os import path
-        sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-        from context import kernel_tuner
-    else:
-        from .context import kernel_tuner
+with open('convolution.cu', 'r') as f:
+    kernel_string = f.read()
 
-    with open('convolution.cu', 'r') as f:
-        kernel_string = f.read()
+problem_size = (4096, 4096)
+size = numpy.prod(problem_size)
+input_size = (problem_size[0]+16) * (problem_size[0]+16)
 
-    problem_size = (4096, 4096)
-    size = numpy.prod(problem_size)
-    input_size = (problem_size[0]+16) * (problem_size[0]+16)
+output = numpy.zeros(size).astype(numpy.float32)
+input = numpy.random.randn(input_size).astype(numpy.float32)
+filter = numpy.random.randn(17*17).astype(numpy.float32)
 
-    output = numpy.zeros(size).astype(numpy.float32)
-    input = numpy.random.randn(input_size).astype(numpy.float32)
-    filter = numpy.random.randn(17*17).astype(numpy.float32)
+args = [output, input, filter]
+tune_params = dict()
+tune_params["block_size_x"] = [16*i for i in range(1,9)]
+tune_params["block_size_y"] = [2**i for i in range(6)]
 
-    args = [output, input, filter]
-    tune_params = dict()
-    tune_params["block_size_x"] = [16*i for i in range(1,9)]
-    tune_params["block_size_y"] = [2**i for i in range(6)]
+tune_params["tile_size_x"] = [2**i for i in range(3)]
+tune_params["tile_size_y"] = [2**i for i in range(3)]
 
-    tune_params["tile_size_x"] = [2**i for i in range(3)]
-    tune_params["tile_size_y"] = [2**i for i in range(3)]
+grid_div_x = ["block_size_x", "tile_size_x"]
+grid_div_y = ["block_size_y", "tile_size_y"]
 
-    grid_div_x = ["block_size_x", "tile_size_x"]
-    grid_div_y = ["block_size_y", "tile_size_y"]
-
-    kernel_tuner.tune_kernel("convolution_kernel", kernel_string,
-        problem_size, args, tune_params,
-        grid_div_y=grid_div_y, grid_div_x=grid_div_x)
+kernel_tuner.tune_kernel("convolution_kernel", kernel_string,
+    problem_size, args, tune_params,
+    grid_div_y=grid_div_y, grid_div_x=grid_div_x)
 
