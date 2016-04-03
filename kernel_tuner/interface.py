e069e0b05845b4d4e408eb2c5d5c9f573aea4214
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2016-04-03 15:18:29 +0200

    also fixed example in docstring

--- kernel_tuner/interface.py
+++ kernel_tuner/interface.py
@@ -57,8 +57,8 @@ The following shows a simple example use of the kernel tuner:
     a = numpy.random.randn(size).astype(numpy.float32)
     b = numpy.random.randn(size).astype(numpy.float32)
     c = numpy.zeros_like(b)
-
-    args = [c, a, b]
+    n = numpy.int32(size)
+    args = [c, a, b, n]
 
     tune_params = dict()
     tune_params["block_size_x"] = [128+64*i for i in range(15)]
