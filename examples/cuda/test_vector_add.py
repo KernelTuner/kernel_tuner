8a28f3a739c0b27166dcb58e01da2992625f64de
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2016-10-26 13:44:50 +0200

    example unit test for a CUDA kernel

---
+++ examples/cuda/test_vector_add.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""Minimal example for a CUDA Kernel unit test with the Kernel Tuner"""
+
+import numpy
+from kernel_tuner import run_kernel
+
+def test_vector_add():
+
+    kernel_string = """
+    __global__ void vector_add(float *c, float *a, float *b, int n) {
+        int i = blockIdx.x * block_size_x + threadIdx.x;
+        if (i<n) {
+            c[i] = a[i] + b[i];
+        }
+    }
+    """
+
+    size = 10000000
+    problem_size = (size, 1)
+
+    a = numpy.random.randn(size).astype(numpy.float32)
+    b = numpy.random.randn(size).astype(numpy.float32)
+    c = numpy.zeros_like(b)
+    n = numpy.int32(size)
+
+    args = [c, a, b, n]
+    params = {"block_size_x": 512}
+
+    answer = run_kernel("vector_add", kernel_string, problem_size, args, params)
+
+    assert numpy.allclose(answer[0], a+b, atol=1e-8)
