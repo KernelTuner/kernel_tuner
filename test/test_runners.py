892f15dca2c54297c4ece4a3189344786adf2d06
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2016-10-26 21:20:37 +0200

    added test for runners

---
+++ test/test_runners.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+
+import numpy
+from .context import kernel_tuner, skip_if_no_cuda_device
+
+
+
+
+def test_random_sample():
+
+    kernel_string = "float test_kernel(float *a) { return 1.0f; }"
+    a = numpy.array([1,2,3]).astype(numpy.float32)
+
+    tune_params = {"block_size_x": range(1,25)}
+    print(tune_params)
+
+    result = kernel_tuner.tune_kernel("test_kernel", kernel_string, (1,1), [a], tune_params, sample=True)
+
+    print(result)
+
+    #check that number of benchmarked kernels is 10% (rounded up)
+    assert len(result.keys()) == 3
+
+    #check all returned results make sense
+    for k,v in result.items():
+        assert int(k) in tune_params.values()[0]
+        assert v == 1.0
+
