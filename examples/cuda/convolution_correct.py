39489603c44232d587796beeeb76516475ccf19b
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2017-02-28 13:14:46 +0100

    set filter dimensions in python script

--- examples/cuda/convolution_correct.py
+++ examples/cuda/convolution_correct.py
@@ -30,20 +30,22 @@ def tune():
     with open('convolution.cu', 'r') as f:
         kernel_string = f.read()
 
+    filter_size = (17, 17)
     problem_size = (4096, 4096)
     size = numpy.prod(problem_size)
-    input_size = ((problem_size[0]+16) * (problem_size[1]+16))
+    border_size = (filter_size[0]//2*2, filter_size[1]//2*2)
+    input_size = ((problem_size[0]+border_size[0]) * (problem_size[1]+border_size[1]))
 
     output = numpy.zeros(size).astype(numpy.float32)
     input = numpy.random.randn(input_size).astype(numpy.float32)
 
-    filter = numpy.random.randn(17*17).astype(numpy.float32)
+    filter = numpy.random.randn(filter_size[0]*filter_size[1]).astype(numpy.float32)
     cmem_args= {'d_filter': filter }
 
     args = [output, input, filter]
     tune_params = OrderedDict()
-    tune_params["filter_width"] = [3]
-    tune_params["filter_height"] = [3]
+    tune_params["filter_width"] = [filter_size[0]]
+    tune_params["filter_height"] = [filter_size[1]]
 
     tune_params["block_size_x"] = [16*i for i in range(1,9)]
     tune_params["block_size_y"] = [2**i for i in range(1,6)]
@@ -59,8 +61,8 @@ def tune():
 
     #compute the answer using a naive kernel
     params = { "block_size_x": 16, "block_size_y": 16}
-    params["filter_width"] = 3
-    params["filter_height"] = 3
+    tune_params["filter_width"] = [filter_size[0]]
+    tune_params["filter_height"] = [filter_size[1]]
     results = kernel_tuner.run_kernel("convolution_naive", kernel_string,
         problem_size, args, params,
         grid_div_y=["block_size_y"], grid_div_x=["block_size_x"])
