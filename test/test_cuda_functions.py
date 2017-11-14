5e01c9f0f3ad7e7eab8a83c3c83ef1bc6e943768
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-11-14 11:29:10 +0100

    Added a test for times with CUDA.

--- test/test_cuda_functions.py
+++ test/test_cuda_functions.py
@@ -78,3 +78,10 @@ def test_benchmark():
     args = [1, 2]
     time = dev.benchmark(test_func, args, (1,2), (1,2), False)
     assert time > 0
+
+def test_benchmark_times():
+    skip_if_no_cuda_device()
+    dev = cuda.CudaFunctions(0)
+    args = [1, 2]
+    time = dev.benchmark(test_func, args, (1,2), (1,2), True)
+    assert time.length == 7
