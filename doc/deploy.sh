a59c9d4526dbea888254ed3a8f950a9e7099ac29
E. G. Patrick Bos <egpbos@gmail.com>
2017-11-15 13:14:26 +0100

    Test whether deploying gh-pages works from PR

--- doc/deploy.sh
+++ doc/deploy.sh
@@ -9,11 +9,11 @@ function doCompile {
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doCompile
-    exit 0
-fi
+# if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+#     echo "Skipping deploy; just doing a build."
+#     doCompile
+#     exit 0
+# fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
