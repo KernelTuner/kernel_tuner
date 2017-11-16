ee29466658ccbcc8349457c66cd5708c59eaff16
Hanno Spreeuw <h.spreeuw@esciencecenter.nl>
2017-11-16 16:27:35 +0100

    First steps in turning the matrix multiplication tutorial into an IPython notebook

--- tutorial/matmul_data_reuse.cu
+++ tutorial/matmul_data_reuse.cu
@@ -1,23 +1,25 @@
+#define WIDTH 4096
+
 __global__ void matmul_kernel(float *C, float *A, float *B) {
 
-    __shared__ float sA[block_size][block_size];
-    __shared__ float sB[block_size][block_size];
+    __shared__ float sA[block_size_y][block_size_x];
+    __shared__ float sB[block_size_y][block_size_x];
 
     int tx = threadIdx.x;
     int ty = threadIdx.y;
-    int x = blockIdx.x * block_size + tx;
-    int y = blockIdx.y * block_size + ty;
+    int x = blockIdx.x * block_size_x + tx;
+    int y = blockIdx.y * block_size_y + ty;
 
     float sum = 0.0;
     int k,kb;
 
-    for (k=0; k<WIDTH; k+=block_size) {
-        __synchthreads();
+    for (k=0; k<WIDTH; k+=block_size_x) {
+        __syncthreads();
         sA[ty][tx] = A[y*WIDTH+k+tx];
         sB[ty][tx] = B[(k+ty)*WIDTH+x];
-        __synchthreads();
+        __syncthreads();
 
-        for (kb=0; kb<block_size; kb++) {
+        for (kb=0; kb<block_size_x; kb++) {
             sum += sA[ty][kb] * sB[kb][tx];
         }
 
