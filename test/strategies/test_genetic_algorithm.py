ac1353500258031cc6585eed6453c6114de39fc8
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2019-10-28 17:20:48 +0100

    expand tests for genetic algorithm

--- test/strategies/test_genetic_algorithm.py
+++ test/strategies/test_genetic_algorithm.py
@@ -14,25 +14,32 @@ def test_weighted_choice():
     dna_size = 2
     pop_size = 5
     pop = ga.random_population(pop_size, tune_params)
-    wpop = [[p, i] for i,p in enumerate(pop)]
+    weighted_pop = [[p, i] for i,p in enumerate(pop)]
 
-    result = ga.weighted_choice(wpop, 1)
+    result = ga.weighted_choice(weighted_pop, 1)
     assert result[0] in pop
 
-    result = ga.weighted_choice(wpop, 2)
+    result = ga.weighted_choice(weighted_pop, 2)
+    print(result)
     assert result[0] in pop
     assert result[1] in pop
     assert result[0] != result[1]
 
 def test_random_population():
     dna_size = 2
-    pop_size = 5
+    pop_size = 8
     pop = ga.random_population(pop_size, tune_params)
 
     assert len(pop) == pop_size
     assert len(pop[0]) == 2
     assert pop[0][0] in tune_params["x"]
     assert pop[0][1] in tune_params["y"]
+
+    # test all members are unique
+    for i,dna1 in enumerate(pop):
+        for j,dna2 in enumerate(pop):
+            if i != j:
+                assert dna1 != dna2
 
 def test_random_val():
     val0 = ga.random_val(0, tune_params)
@@ -58,6 +65,8 @@ def test_crossover_functions():
              ga.disruptive_uniform_crossover]
     for func in funcs:
         children = func(dna1, dna2)
+        print(dna1, dna2)
+        print(children)
         assert len(children) == 2
         assert len(children[0]) == 3
         assert len(children[1]) == 3
@@ -68,6 +77,7 @@ def test_disruptive_uniform_crossover():
     dna2 = ["x", "x", 1, 2, 3, 4, 7]
     #confirm that disruptive uniform crossover indeed guarantees
     #offsping that is different from the parents and each other
+    #when there is more than 1 difference between the parents
     child1, child2 = ga.disruptive_uniform_crossover(dna1, dna2)
     assert child1 != dna1 and child1 != dna2
     assert child2 != dna1 and child2 != dna2
