9ada09bbc3c4f8bd158bba75aa2617304a5b0412
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2020-11-13 16:44:45 +0100

    Fixing bug in indexing.

--- examples/cuda/vector_add_jinja.cu
+++ examples/cuda/vector_add_jinja.cu
@@ -4,13 +4,13 @@ extern "C"
 {
 __global__ void vector_add(const {{ real_type }} * a,const {{ real_type }} * b, {{ real_type }} * c, int size)
 {
-    int i = (blockIdx.x * block_size_x) + threadIdx.x;
+    int index = (blockIdx.x * {{ block_size_x }} * {{ tiling_x }}) + threadIdx.x;
 
     {% for tile in range(tiling_x) %}
     {% set offset = block_size_x * tile %}
-    if ( i + {{ offset }} < (size / {{ vector_size }}) )
+    if ( index + {{ offset }} < (size / {{ vector_size }}) )
     {
-        c[i + {{ offset }}] = a[i + {{ offset }}] + b[i + {{ offset }}];
+        c[index + {{ offset }}] = a[index + {{ offset }}] + b[index + {{ offset }}];
     }
     {% endfor %}
 }
