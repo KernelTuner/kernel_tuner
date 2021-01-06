cd844f5e42480ec2bd18c56e9832a3b760eeef68
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2021-01-06 10:52:02 +0100

    Got rid of helper functions for vector operations.

--- examples/cuda/vector_add_jinja.cu
+++ examples/cuda/vector_add_jinja.cu
@@ -1,8 +1,5 @@
-#include <helper_math.h>
 
-extern "C"
-{
-__global__ void vector_add(const {{ real_type }} * a,const {{ real_type }} * b, {{ real_type }} * c, int size)
+__global__ void vector_add(const {{ real_type }} * __restrict__ a, const {{ real_type }} * __restrict__ b, {{ real_type }} * __restrict__ c, int size)
 {
     int index = (blockIdx.x * {{ block_size_x }} * {{ tiling_x }}) + threadIdx.x;
 
@@ -10,8 +7,16 @@ __global__ void vector_add(const {{ real_type }} * a,const {{ real_type }} * b, 
     {% set offset = block_size_x * tile %}
     if ( index + {{ offset }} < (size / {{ vector_size }}) )
     {
-        c[index + {{ offset }}] = a[index + {{ offset }}] + b[index + {{ offset }}];
+        {{ real_type }} item_a = a[index + {{ offset }}];
+        {{ real_type }} item_b = b[index + {{ offset }}];
+
+        {% if vector_size == 1 %}
+        c[index + {{ offset }}] =  item_a + item_b;
+        {% elif vector_size == 2 %}
+        c[index + {{ offset }}] =  make_{{ real_type }}(item_a.x + item_b.x, item_a.y + item_b.y);
+        {% elif vector_size == 4 %}
+        c[index + {{ offset }}] =  make_{{ real_type }}(item_a.x + item_b.x, item_a.y + item_b.y, item_a.z + item_b.z, item_a.w + item_b.w);
+        {% endif %}
     }
     {% endfor %}
-}
 }
