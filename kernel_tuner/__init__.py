246e5c2c9a59b0c641c24d265a517d927e3aa248
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2021-01-14 09:34:31 +0100

    add create device targets

--- kernel_tuner/__init__.py
+++ kernel_tuner/__init__.py
@@ -1,5 +1,5 @@
 
-from kernel_tuner.integration import store_results
+from kernel_tuner.integration import store_results, create_device_targets
 
 from kernel_tuner.interface import tune_kernel, run_kernel
 
