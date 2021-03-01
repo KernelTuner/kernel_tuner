633d142093e3356c2cd204e856ff200311ceb04f
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2021-03-01 10:20:44 +0100

    fix security warning and simplify example

--- examples/cuda/vector_add_jinja.cu
+++ examples/cuda/vector_add_jinja.cu
@@ -1,5 +1,7 @@
+{% set real_vector = real_type ~ vector_size %}
+#define float1 float
 
-__global__ void vector_add(const {{ real_type }} * __restrict__ a, const {{ real_type }} * __restrict__ b, {{ real_type }} * __restrict__ c, int size)
+__global__ void vector_add(const {{ real_vector }} * __restrict__ a, const {{ real_vector }} * __restrict__ b, {{ real_vector }} * __restrict__ c, int size)
 {
     int index = (blockIdx.x * {{ block_size_x }} * {{ tiling_x }}) + threadIdx.x;
 
@@ -7,15 +9,15 @@ __global__ void vector_add(const {{ real_type }} * __restrict__ a, const {{ real
     {% set offset = block_size_x * tile %}
     if ( index + {{ offset }} < (size / {{ vector_size }}) )
     {
-        {{ real_type }} item_a = a[index + {{ offset }}];
-        {{ real_type }} item_b = b[index + {{ offset }}];
+        {{ real_vector }} item_a = a[index + {{ offset }}];
+        {{ real_vector }} item_b = b[index + {{ offset }}];
 
         {% if vector_size == 1 %}
         c[index + {{ offset }}] =  item_a + item_b;
         {% elif vector_size == 2 %}
-        c[index + {{ offset }}] =  make_{{ real_type }}(item_a.x + item_b.x, item_a.y + item_b.y);
+        c[index + {{ offset }}] =  make_{{ real_vector }}(item_a.x + item_b.x, item_a.y + item_b.y);
         {% elif vector_size == 4 %}
-        c[index + {{ offset }}] =  make_{{ real_type }}(item_a.x + item_b.x, item_a.y + item_b.y, item_a.z + item_b.z, item_a.w + item_b.w);
+        c[index + {{ offset }}] =  make_{{ real_vector }}(item_a.x + item_b.x, item_a.y + item_b.y, item_a.z + item_b.z, item_a.w + item_b.w);
         {% endif %}
     }
     {% endfor %}
--- examples/cuda/vector_add_jinja2.py
+++ examples/cuda/vector_add_jinja2.py
@@ -1,25 +1,19 @@
 """
 More advanced example of vector_add using a Jinja2 template.
 """
+import json
+
+import numpy
+from jinja2 import Template
 
 from kernel_tuner import tune_kernel
-from jinja2 import Environment, FileSystemLoader
-import json
-import numpy
-
-
-def generate_code(tuning_parameters):
-    template_loader = FileSystemLoader(".")
-    template_environment = Environment(loader=template_loader)
-    template = template_environment.get_template("vector_add_jinja.cu")
-    if tuning_parameters["vector_size"] == 1:
-        real_type = tuning_parameters["real_type"]
-    else:
-        real_type = str(tuning_parameters["real_type"]) + str(tuning_parameters["vector_size"])
-    return template.render(real_type=real_type, vector_size=tuning_parameters["vector_size"], block_size_x=tuning_parameters["block_size_x"], tiling_x=tuning_parameters["tiling_x"])
 
 
 def tune():
+
+    with open('vector_add_jinja.cu', 'r') as template_file:
+        template = Template(template_file.read())
+
     size = 10000000
 
     a = numpy.random.randn(size).astype(numpy.float32)
@@ -36,7 +30,7 @@ def tune():
     tuning_parameters["vector_size"] = [1, 2, 4]
     tuning_parameters["tiling_x"] = [i for i in range(1, 33)]
 
-    result = tune_kernel("vector_add", generate_code, size, arguments, tuning_parameters, lang="CUDA", grid_div_x=["block_size_x * vector_size * tiling_x"], answer=control)
+    result = tune_kernel("vector_add", template.render, size, arguments, tuning_parameters, lang="CUDA", grid_div_x=["block_size_x * vector_size * tiling_x"], answer=control)
 
     with open("vector_add_jinja.json", 'w') as fp:
         json.dump(result, fp)
