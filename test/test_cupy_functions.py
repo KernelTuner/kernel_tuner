5b6e67536e12eaced1bf8a526eb21197c1dfdd9f
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2021-11-12 16:22:00 +0100

    add skipmark

--- test/test_cupy_functions.py
+++ test/test_cupy_functions.py
@@ -3,7 +3,7 @@ import kernel_tuner
 from .context import skip_if_no_cupy
 from .test_runners import env
 
-
+@skip_if_no_cupy
 def test_tune_kernel(env):
     result, _ = kernel_tuner.tune_kernel(*env, lang="cupy", verbose=True)
     assert len(result) > 0
