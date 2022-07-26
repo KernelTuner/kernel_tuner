cd5e0dad59f26b4c1a1b2ed29f9a5e500d25e629
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2022-07-26 13:44:44 +0200

    Adding mocked functions.

---
+++ test/test_cuda_mocked.py
@@ -0,0 +1,96 @@
+import numpy as np
+
+try:
+    from mock import patch, Mock
+except ImportError:
+    from unittest.mock import patch, Mock
+
+from kernel_tuner import nvcuda
+from kernel_tuner.core import KernelSource, KernelInstance
+
+
+def setup_mock(drv):
+    context = Mock()
+    devprops = {'MAX_THREADS_PER_BLOCK': 1024,
+                'COMPUTE_CAPABILITY_MAJOR': 5,
+                'COMPUTE_CAPABILITY_MINOR': 5}
+    context.return_value.get_device.return_value.get_attributes.return_value = devprops
+    context.return_value.get_device.return_value.compute_capability.return_value = "55"
+    drv.Device.return_value.make_context.return_value = context()
+    drv.mem_alloc.return_value = 'mem_alloc'
+    return drv
+
+
+@patch('kernel_tuner.pycuda.nvml')
+@patch('kernel_tuner.pycuda.DynamicSourceModule')
+@patch('kernel_tuner.pycuda.drv')
+def test_ready_argument_list(drv, *args):
+    drv = setup_mock(drv)
+
+    size = 5
+    a = np.int32(75)
+    b = np.random.randn(size).astype(np.float32)
+    arguments = [a, b]
+
+    with nvcuda.CudaFunctions(0) as dev:
+        gpu_args = dev.ready_argument_list(arguments)
+
+    print(drv.mock_calls)
+    print(gpu_args)
+
+    drv.mem_alloc.assert_called_once_with(20)
+    drv.memcpy_htod.assert_called_once_with('mem_alloc', b)
+
+    assert isinstance(gpu_args[0], np.int32)
+
+
+@patch('kernel_tuner.pycuda.nvml')
+@patch('kernel_tuner.pycuda.DynamicSourceModule')
+@patch('kernel_tuner.pycuda.drv')
+def test_compile(drv, *args):
+
+    # setup mocked stuff
+    drv = setup_mock(drv)
+    with nvcuda.CudaFunctions(0) as dev:
+        dev.source_mod = Mock()
+        dev.source_mod.return_value.get_function.return_value = 'func'
+
+        # call compile
+        kernel_string = "__global__ void vector_add()"
+        kernel_name = "vector_add"
+        kernel_sources = KernelSource(kernel_name, kernel_string, "cuda")
+        kernel_instance = KernelInstance(kernel_name, kernel_sources, kernel_string, [], None, None, dict(), [])
+        func = dev.compile(kernel_instance)
+
+        # verify behavior
+        assert dev.source_mod.call_count == 1
+        assert dev.current_module is dev.source_mod.return_value
+        assert func == 'func'
+
+        assert kernel_string == list(dev.source_mod.mock_calls[0])[1][0]
+        optional_args = list(dev.source_mod.mock_calls[0])[2]
+        assert optional_args['code'] == 'sm_55'
+        assert optional_args['arch'] == 'compute_55'
+
+
+def dummy_func(a, b, block=0, grid=0, shared=0, stream=None, texrefs=None):
+    pass
+
+
+@patch('kernel_tuner.pycuda.nvml')
+@patch('kernel_tuner.pycuda.DynamicSourceModule')
+@patch('kernel_tuner.pycuda.drv')
+def test_copy_constant_memory_args(drv, *args):
+    drv = setup_mock(drv)
+
+    fake_array = np.zeros(10).astype(np.float32)
+    cmem_args = {'fake_array': fake_array}
+
+    with nvcuda.CudaFunctions(0) as dev:
+        dev.current_module = Mock()
+        dev.current_module.get_global.return_value = ['get_global']
+
+        dev.copy_constant_memory_args(cmem_args)
+
+        drv.memcpy_htod.assert_called_once_with('get_global', fake_array)
+        dev.current_module.get_global.assert_called_once_with('fake_array')
