0be64c864913531546728779d291419e66325e42
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2022-08-02 14:38:13 +0200

    Missing extern.

--- examples/cuda/vector_add_nvcuda.py
+++ examples/cuda/vector_add_nvcuda.py
@@ -8,7 +8,7 @@ from kernel_tuner import tune_kernel
 def tune():
 
     kernel_string = """
-    __global__ void vector_add(float *c, float *a, float *b, int n) {
+    extern "C" __global__ void vector_add(float *c, float *a, float *b, int n) {
         int i = blockIdx.x * block_size_x + threadIdx.x;
         if (i<n) {
             c[i] = a[i] + b[i];
