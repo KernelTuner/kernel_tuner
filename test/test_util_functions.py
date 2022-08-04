738e2597a2e879eeb439f8f1c3b825cbb0c3201d
stijn <s.heldens@esciencecenter.nl>
2022-08-04 15:02:59 +0200

    Fix test that was broken by 767ad4948f98f

--- test/test_util_functions.py
+++ test/test_util_functions.py
@@ -167,13 +167,13 @@ def test_prepare_kernel_string():
     params["is"] = 8
 
     _, output = prepare_kernel_string("this", kernel, params, grid, threads, block_size_names, "", None)
-    expected = "#define kernel_tuner 1\n" \
-               "#define is 8\n" \
-               "#define block_size_z 3\n" \
-               "#define block_size_y 2\n" \
-               "#define block_size_x 1\n" \
+    expected = "#define grid_size_x 3\n" \
                "#define grid_size_y 7\n" \
-               "#define grid_size_x 3\n" \
+               "#define block_size_x 1\n" \
+               "#define block_size_y 2\n" \
+               "#define block_size_z 3\n" \
+               "#define is 8\n" \
+               "#define kernel_tuner 1\n" \
                "#line 1\n" \
                "this is a weird kernel"
     assert output == expected
@@ -185,9 +185,9 @@ def test_prepare_kernel_string():
         baz=lambda config: config["is"] * 5)
 
     _, output = prepare_kernel_string("this", kernel, params, grid, threads, block_size_names, "", defines)
-    expected = "#define baz 40\n" \
+    expected = "#define foo 1\n" \
                "#define bar custom\n" \
-               "#define foo 1\n" \
+               "#define baz 40\n" \
                "#line 1\n" \
                "this is a weird kernel"
     assert output == expected
