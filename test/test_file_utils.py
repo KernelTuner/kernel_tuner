42a49079cb936bd585f6e85776ad6610f1f0d60c
fjwillemsen <fjwillemsen@icloud.com>
2022-12-23 13:41:59 +0100

    Changed test_store_output_file to use real cachefile, added timestamps to cachefile, simplified imports in test_runners

--- test/test_file_utils.py
+++ test/test_file_utils.py
@@ -1,14 +1,16 @@
 from kernel_tuner.file_utils import store_output_file, store_metadata_file, output_file_schema, validate
 from .test_integration import fake_results
+from .test_runners import env, cache_filename, tune_kernel
 import pytest
 import json
 import os
 
 
-def test_store_output_file(fake_results):
+def test_store_output_file(env):
     # setup variables
     filename = "test_output_file.json"
-    _, _, tune_params, _, _, results, _ = fake_results
+    results, _ = tune_kernel(*env, cache=cache_filename, simulation_mode=True)
+    tune_params = env[-1]
 
     # run store_output_file
     store_output_file(filename, results, tune_params)
