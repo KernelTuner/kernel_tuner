ec215d0d8f60c079abb4f883a5b5c33b8f4fee70
schoonhovenrichard <r.a.schoonhoven@hotmail.com>
2023-04-19 14:53:25 +0200

    Update matmul example

--- examples/cuda/chatgpt_queries.py
+++ examples/cuda/chatgpt_queries.py
@@ -2,14 +2,14 @@ chatGPT_queries = {
     "start": lambda kernel_string:
         f"""
         """,
-    "vary_work_per_thread" : {
-        "Initial_2elem": lambda kernel_string:
+    "vary_work_per_thread_x" : {
+        "Initial_2elem_x": lambda kernel_string:
             f"""
-            Rewrite the following CUDA code:
+            Rewrite the following CUDA code to process 2 elements in the x dimension:
             {kernel_string}.
             Do not change the argument list of the kernel.
             First write "START", then write the rewritten kernel and write "END" straight after the kernel. """,
-        "Tunable_nr_elem": lambda:
+        "Tunable_nr_elem_x": lambda:
             f"""
             Please rewrite the code introducing a for loop to allow the number of elements processed by each thread to vary. This number is specified using a C preprocessor defined constant? Please call this constant 'tile_size_x' in lower case. You can omit the definition of this C preprocessor constant.
             """,
@@ -23,7 +23,7 @@ chatGPT_queries = {
             """,
         "Fails_to_compile": lambda error:
             f"""
-This kernel does not compile for me, I get the error:
+This kernel is not correct, I get the error:
 {error}
 Can you try again?
             """,
