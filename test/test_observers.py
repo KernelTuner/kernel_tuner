943b3c470c72bde8141ee40a5c4647aad0050280
fjwillemsen <fjwillemsen@icloud.com>
2024-02-08 17:24:36 +0100

    Added test for RegisterObserver, added clause in case of mocktest

--- test/test_observers.py
+++ test/test_observers.py
@@ -2,9 +2,10 @@
 
 import kernel_tuner
 from kernel_tuner.observers.nvml import NVMLObserver
+from kernel_tuner.observers.register import RegisterObserver
 from kernel_tuner.observers.observer import BenchmarkObserver
 
-from .context import skip_if_no_pycuda, skip_if_no_pynvml
+from .context import skip_if_no_pycuda, skip_if_no_pynvml, skip_if_no_cupy, skip_if_no_cuda
 from .test_runners import env  # noqa: F401
 
 
@@ -20,6 +21,29 @@ def test_nvml_observer(env):
     assert "temperature" in result[0]
     assert result[0]["temperature"] > 0
 
+@skip_if_no_pycuda
+def test_register_observer_pycuda(env):
+    registerobserver = RegisterObserver()
+    env[-1]["block_size_x"] = [128]
+    result, _ = kernel_tuner.tune_kernel(*env, observers=[registerobserver], lang='CUDA')
+    assert "num_regs" in result[0]
+    assert result[0]["num_regs"] > 0
+
+@skip_if_no_cupy
+def test_register_observer_cupy(env):
+    registerobserver = RegisterObserver()
+    env[-1]["block_size_x"] = [128]
+    result, _ = kernel_tuner.tune_kernel(*env, observers=[registerobserver], lang='CuPy')
+    assert "num_regs" in result[0]
+    assert result[0]["num_regs"] > 0
+
+@skip_if_no_cuda
+def test_register_observer_nvcuda(env):
+    registerobserver = RegisterObserver()
+    env[-1]["block_size_x"] = [128]
+    result, _ = kernel_tuner.tune_kernel(*env, observers=[registerobserver], lang='NVCUDA')
+    assert "num_regs" in result[0]
+    assert result[0]["num_regs"] > 0
 
 @skip_if_no_pycuda
 def test_custom_observer(env):
