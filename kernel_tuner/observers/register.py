1681730399f91d2143c0f7630b0c8e87a5438b42
fjwillemsen <fjwillemsen@icloud.com>
2024-02-09 11:43:26 +0100

    Added useful error message in case Register Observer is not supported

--- kernel_tuner/observers/register.py
+++ kernel_tuner/observers/register.py
@@ -7,6 +7,10 @@ class RegisterObserver(BenchmarkObserver):
         super().__init__()
 
     def get_results(self):
+        try:
+            registers_per_thread = self.dev.num_regs
+        except AttributeError:
+            raise NotImplementedError(f"Backend '{type(self.dev).__name__}' does not support count of registers per thread")
         return {
-            "num_regs": self.dev.num_regs
+            "num_regs": registers_per_thread
         }
