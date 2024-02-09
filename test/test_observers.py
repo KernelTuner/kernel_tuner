f153945b0f2c6a7a55d2dde4fa3f6ad1a8ce310e
fjwillemsen <fjwillemsen@icloud.com>
2024-02-09 11:44:11 +0100

    Added tests for Register Observer for OpenCL and HIP backends

--- test/test_observers.py
+++ test/test_observers.py
@@ -1,12 +1,14 @@
-
-
 import kernel_tuner
 from kernel_tuner.observers.nvml import NVMLObserver
 from kernel_tuner.observers.register import RegisterObserver
 from kernel_tuner.observers.observer import BenchmarkObserver
 
-from .context import skip_if_no_pycuda, skip_if_no_pynvml, skip_if_no_cupy, skip_if_no_cuda
+from .context import skip_if_no_pycuda, skip_if_no_pynvml, skip_if_no_cupy, skip_if_no_cuda, skip_if_no_opencl, skip_if_no_pyhip
 from .test_runners import env  # noqa: F401
+from .test_opencl_functions import env as env_opencl    # noqa: F401
+from .test_hip_functions import env as env_hip          # noqa: F401
+
+from pytest import raises
 
 
 @skip_if_no_pycuda
@@ -22,30 +24,6 @@ def test_nvml_observer(env):
     assert result[0]["temperature"] > 0
 
 @skip_if_no_pycuda
-def test_register_observer_pycuda(env):
-    registerobserver = RegisterObserver()
-    env[-1]["block_size_x"] = [128]
-    result, _ = kernel_tuner.tune_kernel(*env, observers=[registerobserver], lang='CUDA')
-    assert "num_regs" in result[0]
-    assert result[0]["num_regs"] > 0
-
-@skip_if_no_cupy
-def test_register_observer_cupy(env):
-    registerobserver = RegisterObserver()
-    env[-1]["block_size_x"] = [128]
-    result, _ = kernel_tuner.tune_kernel(*env, observers=[registerobserver], lang='CuPy')
-    assert "num_regs" in result[0]
-    assert result[0]["num_regs"] > 0
-
-@skip_if_no_cuda
-def test_register_observer_nvcuda(env):
-    registerobserver = RegisterObserver()
-    env[-1]["block_size_x"] = [128]
-    result, _ = kernel_tuner.tune_kernel(*env, observers=[registerobserver], lang='NVCUDA')
-    assert "num_regs" in result[0]
-    assert result[0]["num_regs"] > 0
-
-@skip_if_no_pycuda
 def test_custom_observer(env):
     env[-1]["block_size_x"] = [128]
 
@@ -58,3 +36,34 @@ def test_custom_observer(env):
     assert "name" in result[0]
     assert len(result[0]["name"]) > 0
 
+@skip_if_no_pycuda
+def test_register_observer_pycuda(env):
+    result, _ = kernel_tuner.tune_kernel(*env, observers=[RegisterObserver()], lang='CUDA')
+    assert "num_regs" in result[0]
+    assert result[0]["num_regs"] > 0
+
+@skip_if_no_cupy
+def test_register_observer_cupy(env):
+    result, _ = kernel_tuner.tune_kernel(*env, observers=[RegisterObserver()], lang='CuPy')
+    assert "num_regs" in result[0]
+    assert result[0]["num_regs"] > 0
+
+@skip_if_no_cuda
+def test_register_observer_nvcuda(env):
+    result, _ = kernel_tuner.tune_kernel(*env, observers=[RegisterObserver()], lang='NVCUDA')
+    assert "num_regs" in result[0]
+    assert result[0]["num_regs"] > 0
+
+@skip_if_no_opencl
+def test_register_observer_opencl(env_opencl):
+    with raises(NotImplementedError) as err:
+        kernel_tuner.tune_kernel(*env_opencl, observers=[RegisterObserver()], lang='OpenCL')
+    assert err.errisinstance(NotImplementedError)
+    assert "OpenCL" in str(err.value)
+
+@skip_if_no_pyhip
+def test_register_observer_hip(env_opencl):
+    with raises(NotImplementedError) as err:
+        kernel_tuner.tune_kernel(*env_opencl, observers=[RegisterObserver()], lang='HIP')
+    assert err.errisinstance(NotImplementedError)
+    assert "Hip" in str(err.value)
