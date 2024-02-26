d73c6997db6c3c46b84f42953d42ba55ffdd18cf
WarNIO <me.warnio@gmail.com>
2024-02-26 20:17:52 +0100

    Remove old jsonschema directory and move tests

---
+++ test/test_cache_file.py
@@ -0,0 +1,61 @@
+import json
+from pathlib import Path, PurePath
+import glob
+
+import pytest
+import jsonschema
+
+
+PROJECT_DIR = Path(__file__).parents[1]
+
+SCHEMA_PATH = PROJECT_DIR / "kernel_tuner/schema/cache/1.0.0/schema.json"
+TEST_CACHE_PATH = PROJECT_DIR / "test/test_cache_files/"
+
+
+def get_cache_name(path: str):
+    path = PurePath(path)
+    return str(path.relative_to(path.parents[1]))
+
+
+VALID_CACHE_PATHS = glob.glob(str(TEST_CACHE_PATH / "*/valid_*.json"))
+VALID_CACHE_NAMES = [get_cache_name(p) for p in VALID_CACHE_PATHS]
+
+INVALID_CACHE_PATHS = glob.glob(str(TEST_CACHE_PATH / "*/invalid_*.json"))
+INVALID_CACHE_NAMES = [get_cache_name(p) for p in INVALID_CACHE_PATHS]
+
+
+@pytest.fixture(scope="session")
+def validator():
+    with open(SCHEMA_PATH) as f:
+        schema_json = json.load(f)
+        jsonschema.Draft202012Validator.check_schema(schema_json)
+        return jsonschema.Draft202012Validator(schema_json)
+
+
+@pytest.fixture(
+    scope="session",
+    params=VALID_CACHE_PATHS,
+    ids=VALID_CACHE_NAMES)
+def valid_cache(request):
+    cache_path = request.param
+    with open(cache_path) as f:
+        return json.load(f)
+
+
+@pytest.fixture(
+    scope="session",
+    params=INVALID_CACHE_PATHS,
+    ids=INVALID_CACHE_NAMES)
+def invalid_cache(request):
+    cache_path = request.param
+    with open(cache_path) as f:
+        return json.load(f)
+
+
+def test_valid_cache(validator, valid_cache):
+    validator.validate(valid_cache)
+
+
+def test_invalid_cache(validator, invalid_cache):
+    with pytest.raises(jsonschema.exceptions.ValidationError):
+        validator.validate(invalid_cache)
