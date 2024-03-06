63c0918bda544c474e55c3e43209d956cd6ecd63
Tobias <tobias@Ovidius>
2024-03-06 13:38:18 +0100

    Added support for default conversions; need to implement default conversion function

--- kernel_tuner/convert/convert.py
+++ kernel_tuner/convert/convert.py
@@ -12,22 +12,28 @@ def convert_cache_file(filestr):
     #version = cache_data["version"]
     version = "1.0.0"
 
-    version = version.replace(".", "_")
+    # Obtain a sorted list of all versions
+    versions = list(map (lambda v:v.name, sorted(SCHEMA_VERSIONS_PATH.iterdir())))
 
-    versions = sorted(SCHEMA_VERSIONS_PATH.iterdir())
-    versions = list(map (lambda v:v.name,              versions))
-    versions = list(map (lambda v:v.replace(".", "_"), versions))
-
-
+    func_list = globals()
 
     for v in range(len(versions)-1):
-        if versions[v] == version:
-            func = "c" + versions[v] + "_to_" + versions[v+1]
-            print("Calling {}".format(func))
-            globals()[func](filestr)
+        oldver = versions[v]
+        newver = versions[v+1]
+        
+        if oldver == version:
+            func = ("c" + oldver + "_to_" + newver).replace(".", "_")
+
+            if func in func_list:
+                print("Calling {}".format(func))
+                func_list[func](filestr)
+            else:
+                print("Calling default conversion")
+                default_convert(filestr, oldver, newver)
+
             version = versions[v+1]
 
-    print("Cache file converted to version {}".format(version.replace("_", ".")))
+    print("Cache file converted to version {}".format(version))
     return
 
 
@@ -39,18 +45,23 @@ def c1_0_0_to_1_1_0(filestr):
     print("Function c1_0_0_to_1_1_0 called")
     return
 
-
+"""
 def c1_1_0_to_1_1_1(filestr):
     # Convert
     print("Function c1_0_0_to_1_1_1 called")
     return
-
+"""
 
 def c1_1_1_to_1_2_0(filestr):
     # Convert
     print("Function c1_1_0_to_1_2_0 called")
     return
 
+
+def default_convert(filestr, oldver, newver):
+    # Convert
+    print("Attempting default convert")
+    return
 
 
 convert_cache_file("abc")
