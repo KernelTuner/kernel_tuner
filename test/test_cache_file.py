d98ba80d8cd5b1c23141b52ade9c57991806ad3c
WarNIO <me.warnio@gmail.com>
2024-03-07 12:13:42 +0100

    Simplify tests

--- test/test_cache_file.py
+++ test/test_cache_file.py
@@ -1,65 +1,119 @@
 import json
-from pathlib import Path, PurePath
-import glob
-import os
+from pathlib import Path
+from copy import deepcopy
+
+import sys
+print(sys.path)  # noqa
+
+from kernel_tuner.cache.json import CacheFileJSON
 
 import pytest
 import jsonschema
 
 
-def get_cache_name(path: os.PathLike):
-    path = PurePath(path)
-    return str(path.relative_to(TEST_CACHE_PATH))
-
-
-def recursive_glob(path: os.PathLike):
-    return glob.glob(path.__fspath__(), recursive=True)
-
-
 PROJECT_DIR = Path(__file__).parents[1]
-
 SCHEMA_PATH = PROJECT_DIR / "kernel_tuner/schema/cache/1.0.0/schema.json"
 TEST_CACHE_PATH = PROJECT_DIR / "test/test_cache_files"
+SMALL_CACHE_PATH = TEST_CACHE_PATH / "small_cache.json"
+LARGE_CACHE_PATH = TEST_CACHE_PATH / "large_cache.json"
 
-VALID_CACHE_PATHS = recursive_glob(TEST_CACHE_PATH / "**/valid_*.json")
-VALID_CACHE_NAMES = [get_cache_name(p) for p in VALID_CACHE_PATHS]
-
-INVALID_CACHE_PATHS = recursive_glob(TEST_CACHE_PATH / "**/invalid_*.json")
-INVALID_CACHE_NAMES = [get_cache_name(p) for p in INVALID_CACHE_PATHS]
+with open(SMALL_CACHE_PATH) as f:
+    SMALL_CACHE = json.load(f)
+with open(LARGE_CACHE_PATH) as f:
+    LARGE_CACHE = json.load(f)
 
 
 @pytest.fixture(scope="session")
-def validator():
+def validator() -> jsonschema.Draft202012Validator:
     with open(SCHEMA_PATH) as f:
         schema_json = json.load(f)
         jsonschema.Draft202012Validator.check_schema(schema_json)
         return jsonschema.Draft202012Validator(schema_json)
 
 
-@pytest.fixture(
-    scope="session",
-    params=VALID_CACHE_PATHS,
-    ids=VALID_CACHE_NAMES)
-def valid_cache(request):
-    cache_path = request.param
-    with open(cache_path) as f:
-        return json.load(f)
+@pytest.fixture()
+def cache() -> CacheFileJSON:
+    return deepcopy(SMALL_CACHE)
 
 
-@pytest.fixture(
-    scope="session",
-    params=INVALID_CACHE_PATHS,
-    ids=INVALID_CACHE_NAMES)
-def invalid_cache(request):
-    cache_path = request.param
-    with open(cache_path) as f:
-        return json.load(f)
+@pytest.fixture()
+def large(cache: CacheFileJSON):
+    cache.clear()
+    cache.update(deepcopy(LARGE_CACHE))
 
 
-def test_valid_cache(validator, valid_cache):
-    validator.validate(valid_cache)
+@pytest.fixture()
+def is_valid(validator, cache):
+    yield  # let the test apply some modifications
+    validator.validate(cache)  # assert the cache is valid
 
 
-def test_invalid_cache(validator, invalid_cache):
+@pytest.fixture()
+def is_invalid(validator, cache):
+    yield  # let the test apply some modifications
     with pytest.raises(jsonschema.exceptions.ValidationError):
-        validator.validate(invalid_cache)
+        validator.validate(cache)  # assert the cache is invalid
+
+
+class TestCacheFileSchema:
+    def test_small_cache_is_valid(self, cache, is_valid): pass
+
+    def test_large_cache_is_valid(self, large, cache, is_valid): pass
+
+    @pytest.mark.parametrize("key", [
+        "device_name",
+        "kernel_name",
+        "problem_size",
+        "tune_params_keys",
+        "tune_params",
+        "objective",
+        "cache",
+    ])
+    def test_required_keys(self, cache, is_invalid, key):
+        del cache[key]
+
+    @pytest.mark.parametrize("key,value", [
+        ("device_name", 2312),
+        ("kernel_name", True),
+        ("problem_size", 2.5),
+        ("tune_params_keys", {}),
+        ("tune_params", []),
+        ("objective", 4.5),
+        ("cache", []),
+    ])
+    def test_property_types(self, cache, is_invalid, key, value):
+        cache[key] = value
+
+    @pytest.mark.parametrize("index", [0, 1])
+    @pytest.mark.parametrize("key", [
+        "time",
+        "compile_time",
+        "verification_time",
+        "benchmark_time",
+        "strategy_time",
+        "framework_time",
+        "timestamp",
+    ])
+    def test_cache_data_required_keys(self, cache, is_invalid, index, key):
+        cache_keys = list(cache['cache'].keys())
+        cache_key = cache_keys[index]
+        del cache['cache'][cache_key][key]
+
+    @pytest.mark.parametrize("index", [0, 1])
+    @pytest.mark.parametrize("key,value", [
+        ("time", True),
+        ("times", {}),
+        ("times", ["x"]),
+        ("compile_time", None),
+        ("verification_time", 2.5),
+        ("benchmark_time", "Invalid"),
+        ("GFLOP/s", False),
+        ("strategy_time", 100.001),
+        ("framework_time", "15"),
+        ("timestamp", 42),
+    ])
+    def test_cache_data_property_types(self, cache, is_invalid,
+                                       index, key, value):
+        cache_keys = list(cache['cache'].keys())
+        cache_key = cache_keys[index]
+        cache['cache'][cache_key][key] = value
