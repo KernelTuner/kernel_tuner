34564a610e7c0a7338c9ff1d74c9adbeb15a5e32
Tobias H <tobias@huizehofstra.com>
2024-03-08 14:17:48 +0100

    conver cache test now works

--- test/test_convert_cache.py
+++ test/test_convert_cache.py
@@ -7,21 +7,22 @@ import jsonschema
 import kernel_tuner
 
 from kernel_tuner.convert.convert import convert_cache_file
+from kernel_tuner.util import delete_temp_file
 
 
 KERNEL_TUNER_PATH = Path(kernel_tuner.__file__).parent
 SCHEMAS_PATH = KERNEL_TUNER_PATH / "convert/cache"
-SCHEMA_OLD = SCHEMAS_PATH / "1.0.0"
-SCHEMA_NEW = SCHEMAS_PATH / "1.2.0"
+SCHEMA_OLD = SCHEMAS_PATH / "1.0.0/schema.json"
+SCHEMA_NEW = SCHEMAS_PATH / "1.2.0/schema.json"
 
 TEST_PATH = Path(__file__).parent
 TEST_CONVERT_PATH = TEST_PATH / "test_convert_files"
 TEST_CONVERT_FILE = TEST_CONVERT_PATH / "old_cache.json"
-TEST_CONVERT_COPY = TEST_CONVERT_PATH / "new_cache.json"
+TEST_CONVERT_COPY = TEST_CONVERT_PATH / "temp_cache.json"
 
 
 class TestConvertCache:
-    def test_convert():
+    def test_convert(self):
         # The conversion function converts the file it is given, so make a copy
         copyfile(TEST_CONVERT_FILE, TEST_CONVERT_COPY)
 
@@ -37,6 +38,6 @@ class TestConvertCache:
             new_schema = json.load(s)
             jsonschema.validate(cache, new_schema)
 
-        return
+        delete_temp_file(TEST_CONVERT_COPY)
 
-TestConvertCache.test_convert()
+        return
