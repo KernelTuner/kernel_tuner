781839ab2e83d782903f05a19aeaafbf41bcd821
Milo Lurati <mlurati@fs0.das6.cs.vu.nl>
2024-04-08 12:09:00 +0200

    added instances needed for the ensemble down the line of execution

--- kernel_tuner/runners/simulation.py
+++ kernel_tuner/runners/simulation.py
@@ -58,6 +58,10 @@ class SimulationRunner(Runner):
         self.last_strategy_time = 0
         self.units = {}
 
+        self.device_options = device_options # needed for the ensemble strategy down the line
+        self.iterations = iterations # needed for the ensemble strategy down the line
+        self.observers = observers # needed for the ensemble strategy down the line
+
     def get_environment(self, tuning_options):
         env = self.dev.get_environment()
         env["simulation"] = True
