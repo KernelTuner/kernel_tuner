93dce7d3245e7f62ba3ea02d654be40b5973201d
fjwillemsen <fjwillemsen@icloud.com>
2024-04-12 11:53:34 +0200

    Solves various issues with searchspaces and simulation mode

--- kernel_tuner/util.py
+++ kernel_tuner/util.py
@@ -102,9 +102,9 @@ default_block_size_names = [
     "block_size_x",
     "block_size_y",
     "block_size_z",
-    "ngangs",
-    "nworkers",
-    "vlength",
+    # "ngangs",
+    # "nworkers",
+    # "vlength",
 ]
 
 
@@ -263,7 +263,7 @@ def check_restrictions(restrictions, params: dict, verbose: bool) -> bool:
             # if it's a python-constraint, convert to function and execute
             if isinstance(restrict, Constraint):
                 restrict = convert_constraint_restriction(restrict)
-                if not restrict(params.values()):
+                if not restrict(list(params.values())):
                     valid = False
                     break
             # if it's a string, fill in the parameters and evaluate
@@ -284,9 +284,15 @@ def check_restrictions(restrictions, params: dict, verbose: bool) -> bool:
                 # look up the selected parameters and their value
                 selected_params = dict((key, params[key]) for key in selected_params)
                 # call the restriction
-                if not restrict(**selected_params):
-                    valid = False
-                    break
+                if isinstance(restrict, Constraint):
+                    restrict = convert_constraint_restriction(restrict)
+                    if not restrict(list(selected_params.values())):
+                        valid = False
+                        break
+                else:
+                    if not restrict(**selected_params):
+                        valid = False
+                        break
             # otherwise, raise an error
             else:
                 raise ValueError(f"Unkown restriction type {type(restrict)} ({restrict})")
@@ -884,6 +890,7 @@ def parse_restrictions(restrictions: list[str], tune_params: dict, monolithic = 
             except Exception:
                 # it's not a solvable subexpression, return None
                 return None
+            
 
         # either the left or right side of the equation must evaluate to a constant number
         left_num = is_or_evals_to_number(left)
@@ -998,6 +1005,9 @@ def parse_restrictions(restrictions: list[str], tune_params: dict, monolithic = 
             params_used = list(params_used)
             finalized_constraint = None
             if try_to_constraint and " or " not in res and " and " not in res:
+                # if applicable, strip the outermost round brackets
+                while parsed_restriction[0] == '(' and parsed_restriction[-1] == ')' and '(' not in parsed_restriction[1:] and ')' not in parsed_restriction[:1]:
+                    parsed_restriction = parsed_restriction[1:-1]
                 # check if we can turn this into the built-in numeric comparison constraint
                 finalized_constraint = to_numeric_constraint(parsed_restriction, params_used)
                 if finalized_constraint is None:
@@ -1052,8 +1062,15 @@ def compile_restrictions(restrictions: list, tune_params: dict, monolithic = Fal
     # return the restrictions and used parameters
     if len(restrictions_ignore) == 0:
         return compiled_restrictions
-    restrictions_ignore = list(zip(restrictions_ignore, (() for _ in restrictions_ignore)))
-    return restrictions_ignore + compiled_restrictions
+    
+    # use the required parameters or add an empty tuple for unknown parameters of ignored restrictions
+    noncompiled_restrictions = []
+    for r in restrictions_ignore:
+        if isinstance(r, tuple) and len(r) == 2 and isinstance(r[1], (list, tuple)):
+            noncompiled_restrictions.append(r)
+        else:
+            noncompiled_restrictions.append((r, ()))
+    return noncompiled_restrictions + compiled_restrictions
 
 
 def process_cache(cache, kernel_options, tuning_options, runner):
