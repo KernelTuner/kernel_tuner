db3a3d5d0f3c21e4fe27b562843fd4f30b79166a
WarNIO <me.warnio@gmail.com>
2024-04-16 00:10:51 +0200

    Rename variable

--- kernel_tuner/cache/file.py
+++ kernel_tuner/cache/file.py
@@ -2,6 +2,7 @@
 
 import json
 import re
+import os
 from os import PathLike
 
 from kernel_tuner.cache.json_encoder import CacheJSONEncoder
@@ -11,16 +12,16 @@ OPTIONAL_COMMA_END_REGEX = re.compile(r",?$")
 CLOSING_BRACES_REGEX = re.compile(r"}\s*}\s*$")
 
 
-def read_cache_file(file_path: PathLike):
+def read_cache_file(filename: PathLike):
     """Reads a cache file and returns its content as a dictionary.
 
     Parameters:
-        file_path (Path): The path to the cache file.
+        filename (PathLike): The path to the cache file.
 
     Returns:
         dict: The content of the cache file.
     """
-    with open(file_path, "r") as file:
+    with open(filename, "r") as file:
         try:
             return json.load(file)
         except json.JSONDecodeError:
@@ -32,19 +33,32 @@ def read_cache_file(file_path: PathLike):
             text = OPTIONAL_COMMA_END_REGEX.sub("}}", text, 1)
             return json.loads(text)
         except json.JSONDecodeError:
-            raise ValueError(f"Cache file {file_path} is corrupted")
+            raise ValueError(f"Cache file {filename} is corrupted")
 
 
-def write_cache_file(cache_json: dict, file_path: PathLike, *, keep_open=False):
+def write_cache_file(cache_json: dict, filename: PathLike, *, keep_open=False):
     r"""Writes a cache file with the given content.
 
     Parameters:
         cache_file (dict): The content to be written to the cache file.
-        file_path (Path): The path to write the cache file.
+        filename (PathLike): The path to write the cache file.
         keep_open (bool): If true, add a comma instead of the final two braces ('}\n}')
     """
-    with open(file_path, "w") as file:
+    with open(filename, "w") as file:
         text = json.dumps(cache_json, cls=CacheJSONEncoder, indent=0)
         if keep_open:
             text = CLOSING_BRACES_REGEX.sub(",", text)
         file.write(text)
+
+
+def close_cache_file(filename: PathLike):
+    with open(filename, "rb+") as file:
+        file.seek(-1, os.SEEK_END)
+        ch = file.peek(1)
+        while ch.isspace() and ch != b",":
+            if file.tell() == 0:
+                raise ValueError(f"Cache file {filename} is corrupted")
+            file.seek(-1, os.SEEK_CUR)
+            ch = file.peek(1)
+        file.seek(1, os.SEEK_CUR)
+        file.write(b"}\n}")
