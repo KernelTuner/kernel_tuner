ecbaef83f8b9f7aa8fbb5c678d472520ffd1fcae
WarNIO <me.warnio@gmail.com>
2024-04-15 23:21:46 +0200

    Fix cache file reading

--- kernel_tuner/cache/file.py
+++ kernel_tuner/cache/file.py
@@ -8,7 +8,7 @@ from kernel_tuner.cache.json_encoder import CacheJSONEncoder
 
 
 OPTIONAL_COMMA_END_REGEX = re.compile(r",?$")
-CLOSING_BRACES_REGEX = re.compile(r"}\s*}$")
+CLOSING_BRACES_REGEX = re.compile(r"}\s*}\s*$")
 
 
 def read_cache_file(file_path: PathLike):
@@ -26,26 +26,25 @@ def read_cache_file(file_path: PathLike):
         except json.JSONDecodeError:
             pass  # Loading failed
 
-    with open(file_path, "r") as file:
+        file.seek(0)
         try:  # Try to load the file with closing braces appended to it
             text = file.read()
-            text = OPTIONAL_COMMA_END_REGEX.sub("}}", text)
+            text = OPTIONAL_COMMA_END_REGEX.sub("}}", text, 1)
             return json.loads(text)
         except json.JSONDecodeError:
             raise ValueError(f"Cache file {file_path} is corrupted")
 
 
-def write_cache_file(cache_json: dict, file_path: PathLike, *, terminate=True):
-    """Writes a cache file with the given content.
+def write_cache_file(cache_json: dict, file_path: PathLike, *, keep_open=False):
+    r"""Writes a cache file with the given content.
 
     Parameters:
         cache_file (dict): The content to be written to the cache file.
         file_path (Path): The path to write the cache file.
-        terminated (bool): Whether to add the final two accolades ('}')
+        keep_open (bool): If true, add a comma instead of the final two braces ('}\n}')
     """
     with open(file_path, "w") as file:
         text = json.dumps(cache_json, cls=CacheJSONEncoder, indent=0)
-        if not terminate:
-            # TODO: Write test
-            text = CLOSING_BRACES_REGEX.sub("", text)
+        if keep_open:
+            text = CLOSING_BRACES_REGEX.sub(",", text)
         file.write(text)
--- test/test_cachefile_read_write.py
+++ test/test_cachefile_read_write.py
@@ -1,5 +1,4 @@
 import pytest
-import filecmp
 from pathlib import Path
 
 from kernel_tuner.cache.file import read_cache_file, write_cache_file
@@ -7,9 +6,9 @@ from kernel_tuner.cache.file import read_cache_file, write_cache_file
 
 TEST_PATH = Path(__file__).parent
 TEST_CACHE_PATH = TEST_PATH / "test_cache_files"
-CACHE_SAMPLE_PATH = TEST_CACHE_PATH / "convolution_A100.json"
-OPEN_CACHE_PATH_1 = TEST_CACHE_PATH / "open_cache_no_comma.json"
-OPEN_CACHE_PATH_2 = TEST_CACHE_PATH / "open_cache_with_comma.json"
+CLOSED_CACHE_PATH = TEST_CACHE_PATH / "small_cache.json"
+OPEN_CACHE_NO_COMMA_PATH = TEST_CACHE_PATH / "open_cache_no_comma.json"
+OPEN_CACHE_COMMA_PATH = TEST_CACHE_PATH / "open_cache_with_comma.json"
 
 
 @pytest.fixture
@@ -19,21 +18,32 @@ def output_path(tmp_path):
 
 @pytest.mark.parametrize(
     "filename",
-    [CACHE_SAMPLE_PATH, OPEN_CACHE_PATH_1, OPEN_CACHE_PATH_2],
+    [CLOSED_CACHE_PATH, OPEN_CACHE_NO_COMMA_PATH, OPEN_CACHE_COMMA_PATH],
     ids=["closed cache", "open cache without comma", "open cache with comma"],
 )
 def test_read_cache_file(filename):
     # Read device name of the given file
-    file_content = read_cache_file(CACHE_SAMPLE_PATH)
+    file_content = read_cache_file(filename)
     device_name = file_content.get("device_name")
 
     # Check if the expected value of device name is in the file
-    assert device_name == "NVIDIA A100-PCIE-40GB"
+    assert device_name == "Testing"
 
 
 def test_write_cache_file(output_path):
-    sample_cache = read_cache_file(CACHE_SAMPLE_PATH)
+    sample_cache = read_cache_file(CLOSED_CACHE_PATH)
 
     write_cache_file(sample_cache, output_path)
 
-    assert filecmp.cmp(output_path, CACHE_SAMPLE_PATH)
+    with open(output_path, 'r') as output, open(CLOSED_CACHE_PATH, 'r') as input:
+        assert output.read() == input.read()
+
+
+@pytest.mark.skip
+def test_write_open_cache_file(output_path):
+    sample_cache = read_cache_file(OPEN_CACHE_COMMA_PATH)
+
+    write_cache_file(sample_cache, output_path, keep_open=True)
+
+    with open(output_path, 'r') as output, open(OPEN_CACHE_COMMA_PATH, 'r') as input:
+        assert output.read() == input.read()
