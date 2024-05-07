630578253194d3e5662b06811dea654045f9ed11
Milo Lurati <lurati.milo@gmail.com>
2024-05-07 14:51:29 +0200

    unified two actors into one

--- kernel_tuner/runners/ray/remote_actor.py
+++ kernel_tuner/runners/ray/remote_actor.py
@@ -1,11 +1,5 @@
-import logging
-from datetime import datetime, timezone
-from time import perf_counter
 import ray
-import sys
 
-from kernel_tuner.util import ErrorConfig, print_config_output, process_metrics, store_cache
-from kernel_tuner.core import DeviceInterface
 from kernel_tuner.runners.sequential import SequentialRunner
 from kernel_tuner.runners.simulation import SimulationRunner
 
@@ -17,24 +11,41 @@ class RemoteActor():
                  device_options,
                  iterations,
                  observers,
-                 cache_manager):
-        
+                 cache_manager=None,
+                 simulation_mode=False):
         self.kernel_source = kernel_source
-        self.simulation_mode = False
         self.kernel_options = kernel_options
         self.device_options = device_options
         self.iterations = iterations
         self.observers = observers
         self.cache_manager = cache_manager
+        self.simulation_mode = simulation_mode
         self.runner = None
+            
+    def execute(self, tuning_options, strategy=None, searchspace=None, element=None):
+        if self.runner is None:
+            self.init_runner()
+        if strategy and searchspace:
+            results = strategy.tune(searchspace, self.runner, tuning_options)
+            return results, tuning_options
+        elif element:
+            return self.runner.run([element], tuning_options)[0]
+        else:
+            raise ValueError("Invalid arguments for ray actor's execute method.")
         
-    def execute(self, strategy, searchspace, tuning_options, simulation_mode=False):
-        if simulation_mode:
+    def set_cache_manager(self, cache_manager):
+        if self.cache_manager is None:
+            self.cache_manager = cache_manager
+
+    def get_cache_magaer(self):
+        return self.cache_manager
+    
+    def init_runner(self):
+        if self.cache_manager is None:
+            raise ValueError("Cache manager is not set.")
+        if self.simulation_mode:
             self.runner = SimulationRunner(self.kernel_source, self.kernel_options, self.device_options, 
-                                 self.iterations, self.observers)
+                                            self.iterations, self.observers)
         else:
             self.runner = SequentialRunner(self.kernel_source, self.kernel_options, self.device_options, 
-                                 self.iterations, self.observers, cache_manager=self.cache_manager)
-        results = strategy.tune(searchspace, self.runner, tuning_options)
-        return results, tuning_options
-    
+                                       self.iterations, self.observers, cache_manager=self.cache_manager)
