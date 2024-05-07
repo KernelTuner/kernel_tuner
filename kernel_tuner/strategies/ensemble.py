d7fe9b40a919cd6b3f8afbe588ac260a4eb4b393
Milo Lurati <lurati.milo@gmail.com>
2024-05-07 14:55:45 +0200

    updates realted to unification of actors, memetic algo, and reutilization of actors for performance

--- kernel_tuner/strategies/ensemble.py
+++ kernel_tuner/strategies/ensemble.py
@@ -4,13 +4,15 @@ import os
 import ray
 import copy
 import logging
+import warnings
+from collections import deque
 
 import numpy as np
 
 from kernel_tuner import util
 from kernel_tuner.searchspace import Searchspace
 from kernel_tuner.strategies import common
-from kernel_tuner.strategies.common import CostFunc, scale_from_params, check_num_devices
+from kernel_tuner.strategies.common import CostFunc, scale_from_params, check_num_devices, create_actor_on_device, initialize_ray
 from kernel_tuner.runners.simulation import SimulationRunner
 from kernel_tuner.runners.ray.remote_actor import RemoteActor
 from kernel_tuner.util import get_num_devices
@@ -50,40 +52,41 @@ strategy_map = {
     "bayes_opt": bayes_opt,
 }
 
-def tune(searchspace: Searchspace, runner, tuning_options, cache_manager=None):
+def tune(searchspace: Searchspace, runner, tuning_options, cache_manager=None, actors=None):
     simulation_mode = True if isinstance(runner, SimulationRunner) else False
     num_devices = get_num_devices(runner.kernel_source.lang, simulation_mode=simulation_mode)
+    print(f"DEBUG: num_devices={num_devices}", file=sys.stderr)
+    
     ensemble = []
     if "ensemble" in tuning_options:
         ensemble = tuning_options["ensemble"]
     else:
         ensemble = ["greedy_ils", "greedy_ils"]
     ensemble_size = len(ensemble)
+    if num_devices < ensemble_size:
+        warnings.warn("Number of devices is less than the number of strategies in the ensemble. Some strategies will wait until devices are available.", UserWarning)
+    num_actors = num_devices if ensemble_size > num_devices else ensemble_size
     
-    # Initialize Ray
-    if not ray.is_initialized():
-        check_num_devices(ensemble_size, simulation_mode, runner)
-        os.environ["RAY_DEDUP_LOGS"] = "0"
-        ray.init(include_dashboard=True, ignore_reinit_error=True)
+    initialize_ray(num_devices)
     
     # Create cache manager and actors
-    kill_cache_manager = False
     if cache_manager is None:
-        kill_cache_manager = True
         cache_manager = CacheManager.remote(tuning_options)
-    actors = [create_actor(runner, cache_manager, simulation_mode) for _ in range(ensemble_size)]
+    if actors is None:
+        runner_attributes = [runner.kernel_source, runner.kernel_options, runner.device_options, runner.iterations, runner.observers]
+        actors = [create_actor_on_device(*runner_attributes, cache_manager, simulation_mode, id) for id in range(num_actors)]
     
     # Execute all actor with one strategy each
     ensemble = [strategy_map[strategy] for strategy in ensemble]
+    ensemble_queue = deque(ensemble)
     pending_tasks = {}
-    for i in range(ensemble_size):
-        strategy = ensemble[i]
-        actor = actors[i]
+    for actor in actors:
+        strategy = ensemble_queue.popleft()
         remote_tuning_options = setup_tuning_options(tuning_options)
-        task = actor.execute.remote(strategy, searchspace, remote_tuning_options, simulation_mode)
+        task = actor.execute.remote(strategy=strategy, searchspace=searchspace, tuning_options=remote_tuning_options)
         pending_tasks[task] = actor
     
-    # As soon as an actor is done we need to kill it to give space to other actors
+
     all_results = []
     while pending_tasks:
         done_ids, _ = ray.wait(list(pending_tasks.keys()), num_returns=1)
@@ -91,7 +94,12 @@ def tune(searchspace: Searchspace, runner, tuning_options, cache_manager=None):
             result = ray.get(done_id)
             all_results.append(result)
             actor = pending_tasks.pop(done_id)
-            ray.kill(actor)
+
+            if ensemble_queue:
+                strategy = ensemble_queue.popleft()
+                remote_tuning_options = setup_tuning_options(tuning_options)
+                task = actor.execute.remote(strategy=strategy, searchspace=searchspace, tuning_options=remote_tuning_options)
+                pending_tasks[task] = actor
 
     new_tuning_options = ray.get(cache_manager.get_tuning_options.remote())
     tuning_options.update(new_tuning_options)
@@ -102,20 +110,7 @@ def tune(searchspace: Searchspace, runner, tuning_options, cache_manager=None):
     if candidates: # for memetic strategy
         tuning_options.strategy_options["candidates"] = candidates
 
-    clean_up(actors, cache_manager, kill_cache_manager)
     return final_results
-
-def create_actor(runner, cache_manager, simulation_mode):
-    if simulation_mode:
-        resource_options= {"num_cpus": 1}
-    else:
-        resource_options= {"num_gpus": 1}
-    return RemoteActor.options(**resource_options).remote(runner.kernel_source, 
-                                                            runner.kernel_options, 
-                                                            runner.device_options, 
-                                                            runner.iterations, 
-                                                            runner.observers,
-                                                            cache_manager)
 
 def setup_tuning_options(tuning_options):
     new_tuning_options = copy.deepcopy(tuning_options)
@@ -141,9 +136,3 @@ def process_results(all_results, searchspace):
                 final_results.append(new_result)
                 unique_configs.add(config_signature)
     return final_results, population, candidates
-
-def clean_up(actors, cache_manager, kill_cache_manager):
-    for actor in actors:
-        ray.kill(actor)
-    if kill_cache_manager:
-        ray.kill(cache_manager)
