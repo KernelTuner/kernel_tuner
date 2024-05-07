0f2b7e4190833b39b65996f8ea23eb818c28836e
Milo Lurati <lurati.milo@gmail.com>
2024-05-07 14:52:55 +0200

    updates related to actors unification and memetic algo development

--- kernel_tuner/runners/parallel.py
+++ kernel_tuner/runners/parallel.py
@@ -7,14 +7,16 @@ from time import perf_counter
 
 from kernel_tuner.core import DeviceInterface
 from kernel_tuner.runners.runner import Runner
-from kernel_tuner.runners.ray.parallel_remote_actor import ParallelRemoteActor
+from kernel_tuner.runners.ray.remote_actor import RemoteActor
 from kernel_tuner.util import get_num_devices
+from kernel_tuner.runners.ray.cache_manager import CacheManager
+from kernel_tuner.strategies.common import create_actor_on_device, initialize_ray
 
 class ParallelRunner(Runner):
 
-    def __init__(self, kernel_source, kernel_options, device_options, iterations, observers, num_gpus, cache_manager=None):
-        self.dev = DeviceInterface(kernel_source, iterations=iterations, observers=observers, **device_options)
-        self.units = self.dev.units
+    def __init__(self, kernel_source, kernel_options, device_options, iterations, observers, 
+                 num_gpus=None, cache_manager=None, actors=None, simulation_mode=False):
+        self.dev = DeviceInterface(kernel_source, iterations=iterations, observers=observers, **device_options) if not simulation_mode else None
         self.quiet = device_options.quiet
         self.kernel_source = kernel_source
         self.warmed_up = False
@@ -28,11 +30,17 @@ class ParallelRunner(Runner):
         self.device_options = device_options
         self.cache_manager = cache_manager
         self.num_gpus = num_gpus
+        self.actors = actors
 
-        # Initialize Ray
-        if not ray.is_initialized():
-            os.environ["RAY_DEDUP_LOGS"] = "0"
-            ray.init(include_dashboard=True, ignore_reinit_error=True)
+        if num_gpus is None:
+            self.num_gpus = get_num_devices(kernel_source.lang, simulation_mode=self.simulation_mode)
+
+        initialize_ray(num_gpus)
+
+        # Create RemoteActor instances
+        if actors is None:
+            runner_attributes = [self.kernel_source, self.kernel_options, self.device_options, self.iterations, self.observers]
+            self.actors = [create_actor_on_device(*runner_attributes, self.cache_manager, simulation_mode, id) for id in range(self.num_gpus)]
 
     def get_environment(self, tuning_options):
         return self.dev.get_environment()
@@ -41,27 +49,16 @@ class ParallelRunner(Runner):
     def run(self, parameter_space, tuning_options, cache_manager=None):
         if self.cache_manager is None:
             if cache_manager is None:
-                raise ValueError("A cache manager is required for parallel execution")
+                cache_manager = CacheManager.remote(tuning_options)
             self.cache_manager = cache_manager
-        # Create RemoteActor instances
-        self.actors = [self.create_actor_on_gpu(self.cache_manager) for _ in range(self.num_gpus)]
+        # set the cache manager for each actor. Can't be done in constructor because we do not have yet the tuning_options
+        for actor in self.actors:
+            ray.get(actor.set_cache_manager.remote(self.cache_manager))
         # Create a pool of RemoteActor actors
         self.actor_pool = ActorPool(self.actors)
         # Distribute execution of the `execute` method across the actor pool with varying parameters and tuning options, collecting the results asynchronously.
-        results = list(self.actor_pool.map_unordered(lambda a, v: a.execute.remote(v, tuning_options), parameter_space))
+        results = list(self.actor_pool.map_unordered(lambda a, v: a.execute.remote(element=v, tuning_options=tuning_options), parameter_space))
         new_tuning_options = ray.get(self.cache_manager.get_tuning_options.remote())
         tuning_options.update(new_tuning_options)
-
-        for actor in self.actors:
-            ray.kill(actor)
         
         return results
-    
-    def create_actor_on_gpu(self, cache_manager):
-        return ParallelRemoteActor.remote(self.quiet,
-                                            self.kernel_source, 
-                                            self.kernel_options, 
-                                            self.device_options, 
-                                            self.iterations, 
-                                            self.observers,
-                                            cache_manager)
