82f1680ae81c879db38ee3bd3c1f4b3c7ab0a0e2
WarNIO <me.warnio@gmail.com>
2024-06-03 23:30:45 +0200

    Fix undefined `cache` in Python 3.8

--- kernel_tuner/cache/validate.py
+++ kernel_tuner/cache/validate.py
@@ -1,4 +1,4 @@
-""" Validator of the cache files """
+"""Validator of the cache files."""
 
 import json
 import jsonschema
@@ -6,14 +6,13 @@ from datetime import datetime
 from .paths import get_schema_path
 from .versions import VERSIONS, Version
 
-from functools import cache
+from functools import lru_cache
+
 
 def _get_format_checker():
-    """
-    Returns a JSON format checker instance.
-    """
+    """Returns a JSON format checker instance."""
     format_checker = jsonschema.FormatChecker()
-    
+
     @format_checker.checks("date-time")
     def _check_iso_datetime(instance):
         try:
@@ -21,28 +20,28 @@ def _get_format_checker():
             return True
         except ValueError:
             return False
-    
+
     return format_checker
 
-@cache
+
+@lru_cache
 def get_format_checker():
-    """
-    Returns a cached JSON format checker instance.
-    """
+    """Returns a cached JSON format checker instance."""
     return _get_format_checker()
 
+
 def validate_data(data_file):
-    """
-    Validate the input data in a cache file against its corresponding schema.
+    """Validate the input data in a cache file against its corresponding schema.
 
     Parameters:
     data_file (str): The path to the cache file that needs to be validated.
 
     Raises:
-    ValueError: If the cache file does not have a "schema_version" field, or if the version does not match a valid version.
-    FileNotFoundError: If the schema file cannot be found at the specified path.
+    - ValueError:
+      If the cache file does not have a "schema_version" field, or if the version does not match a valid version.
+    - FileNotFoundError: If the schema file cannot be found at the specified path.
     """
-    with open(data_file, 'r') as f:
+    with open(data_file, "r") as f:
         data = json.load(f)
 
     try:
@@ -55,7 +54,7 @@ def validate_data(data_file):
         raise ValueError(f"Schema version '{data['schema_version']}' not found in the data.")
 
     try:
-        with open(schema_path, 'r') as f:
+        with open(schema_path, "r") as f:
             schema = json.load(f)
     except FileNotFoundError as e:
         raise FileNotFoundError(f"Schema file not found at path '{schema_path}': {e}")
