fc89275a050c2b880fd29fe14c4d32d752d3cc92
WarNIO <me.warnio@gmail.com>
2024-06-03 23:32:30 +0200

    Format file and fix error by addin typing

--- kernel_tuner/cache/cli_functionality.py
+++ kernel_tuner/cache/cli_functionality.py
@@ -2,10 +2,10 @@
 
 This is one of:
    - `convert`: one may convert a cachefile to a specified (higher) version.
-   
+
    - `delete-line`: one may delete a certain cacheline from a cachefile; required that the cachefile is of the newest
-       JSON schema version. 
-   - `get-line`: One may obtain the cacheline data of a certain cacheline inside a cachefile. 
+       JSON schema version.
+   - `get-line`: One may obtain the cacheline data of a certain cacheline inside a cachefile.
    - `merge`: one may merge several (with possible non-equivalent version) cachefiles into one cachefile;
       The resulting merged cachefile is always using the **newest** JSON
       schema. Note that the file content of the input files change.
@@ -16,7 +16,7 @@ This is one of:
 
 from os import PathLike
 from shutil import copyfile
-from typing import List
+from typing import List, Any
 
 from .cache import Cache
 from .convert import convert_cache_file, convert_cache_to_t4
@@ -26,7 +26,7 @@ from .versions import LATEST_VERSION
 
 def convert_files_to_their_least_common_version(file_list: List[PathLike]):
     """Converts all necessary files in file_list to the newest schema_version.
-    
+
     Does this by using the existing conversion functionality; convert the file, then write the result to the same file.
     """
     # Get highest schema_version
@@ -38,7 +38,6 @@ def convert_files_to_their_least_common_version(file_list: List[PathLike]):
         if str(cachefile.version) != max_version:
             # We write to the input filename.
             convert_cache_file(filestr=file, target_version=max_version)
-        
 
 
 def check_equivalence(file_list: List[PathLike]):
@@ -49,12 +48,11 @@ def check_equivalence(file_list: List[PathLike]):
     """
     base_file = Cache.read(file_list[0])
 
-
     for file in file_list[1:]:
         temp_file = Cache.read(file)
 
         # Now the equivalence logic
-        
+
         if base_file.version != temp_file.version:
             raise ValueError(f"Merge error; files '{file_list[0]}' and '{file}' do not have the same schema version.")
 
@@ -83,38 +81,41 @@ def merge_files(file_list: List[PathLike], ofile: PathLike):
     # From cache.py (json.load).
 
     resulting_output = Cache.read(file_list[0])
-    resulting_output.create(ofile,
+    resulting_output.create(
+        ofile,
         device_name=resulting_output.device_name,
         kernel_name=resulting_output.kernel_name,
         problem_size=resulting_output.problem_size,
         tune_params_keys=resulting_output.tune_params_keys,
         tune_params=resulting_output.tune_params,
-        objective=resulting_output.objective)
+        objective=resulting_output.objective,
+    )
 
     # We read so the ._filename changes for append
     resulting_output = Cache.read(ofile)
 
     # Now for each file add the cache content.
     for file in file_list:
-
         temp_file = Cache.read(file)
 
         for line in temp_file.lines:
             if line in resulting_output.lines:
                 raise KeyError(f"Merge error; overlap for key '{line}' in several files.")
-            
+
             temp_line = temp_file.lines[line]
             tune_params = {key: temp_line[key] for key in temp_file.tune_params_keys}
-            resulting_output.lines.append(time=temp_line["time"],
-                         compile_time=temp_line["compile_time"],
-                         verification_time=temp_line["verification_time"],
-                         benchmark_time=temp_line["benchmark_time"],
-                         strategy_time=temp_line["strategy_time"],
-                         framework_time=temp_line["framework_time"],
-                         timestamp=temp_file.lines.get(line).timestamp,
-                         times=temp_line["times"],
-                         GFLOP_per_s=temp_line["GFLOP/s"],
-                         **tune_params)
+            resulting_output.lines.append(
+                time=temp_line["time"],
+                compile_time=temp_line["compile_time"],
+                verification_time=temp_line["verification_time"],
+                benchmark_time=temp_line["benchmark_time"],
+                strategy_time=temp_line["strategy_time"],
+                framework_time=temp_line["framework_time"],
+                timestamp=temp_file.lines.get(line).timestamp,
+                times=temp_line["times"],
+                GFLOP_per_s=temp_line["GFLOP/s"],
+                **tune_params,
+            )
 
 
 def get_line(infile: PathLike, key: str):
@@ -123,14 +124,12 @@ def get_line(infile: PathLike, key: str):
 
     cache_line = cache_infile.lines[key]
 
-    print(f"[*] Cacheline entry '{key}' content [*]\n\n"
-          f"************************")
+    print(f"[*] Cacheline entry '{key}' content [*]\n\n" f"************************")
     print(dict(cache_line))
     print("************************")
 
 
-
-def delete_line(infile: PathLike,  key: str, outfile):
+def delete_line(infile: PathLike, key: str, outfile):
     """Tries to remove entry `key` from file `infile`, then write to `outfile` by using the `cache.py` functions.
 
     First we check if the entry actually exists in the cachefile using the library. If not, there
@@ -140,25 +139,27 @@ def delete_line(infile: PathLike,  key: str, outfile):
     if outfile is None:
         outfile = infile
 
-
     cache_infile = Cache.read(infile)
 
     # We require the file to be of the latest version.
     if cache_infile.version != LATEST_VERSION:
-        raise ValueError(f"Cachefile '{infile}' is of version {str(cache_infile.version)} but should be of version "
-                         f"{str(LATEST_VERSION)} (latest).")
+        raise ValueError(
+            f"Cachefile '{infile}' is of version {str(cache_infile.version)} but should be of version "
+            f"{str(LATEST_VERSION)} (latest)."
+        )
 
     if cache_infile.lines.get(key) is None:
         raise KeyError(f"Entry '{key}' is not contained in cachefile '{infile}'.")
 
-    cache_infile.create(outfile,
+    cache_infile.create(
+        outfile,
         device_name=cache_infile.device_name,
         kernel_name=cache_infile.kernel_name,
         problem_size=cache_infile.problem_size,
         tune_params_keys=cache_infile.tune_params_keys,
         tune_params=cache_infile.tune_params,
-        objective=cache_infile.objective)
-
+        objective=cache_infile.objective,
+    )
 
     # We read so the ._filename changes for append
     resulting_output = Cache.read(outfile)
@@ -167,47 +168,43 @@ def delete_line(infile: PathLike,  key: str, outfile):
         if line != key:
             temp_line = cache_infile.lines[line]
             tune_params = {key: temp_line[key] for key in cache_infile.tune_params_keys}
-            resulting_output.lines.append(time=temp_line["time"],
-                         compile_time=temp_line["compile_time"],
-                         verification_time=temp_line["verification_time"],
-                         benchmark_time=temp_line["benchmark_time"],
-                         strategy_time=temp_line["strategy_time"],
-                         framework_time=temp_line["framework_time"],
-                         timestamp=cache_infile.lines.get(line).timestamp,
-                         times=temp_line["times"],
-                         GFLOP_per_s=temp_line["GFLOP/s"],
-                         **tune_params)
+            resulting_output.lines.append(
+                time=temp_line["time"],
+                compile_time=temp_line["compile_time"],
+                verification_time=temp_line["verification_time"],
+                benchmark_time=temp_line["benchmark_time"],
+                strategy_time=temp_line["strategy_time"],
+                framework_time=temp_line["framework_time"],
+                timestamp=cache_infile.lines.get(line).timestamp,
+                times=temp_line["times"],
+                GFLOP_per_s=temp_line["GFLOP/s"],
+                **tune_params,
+            )
 
     print(f"Writing to output file '{outfile}' after removing entry '{key}' completed.")
-
 
 
 def convert(read_file: PathLike, write_file=None, target=None):
     """The main function for handling the version conversion of a cachefile."""
     # Check if the `read_file` is actually a valid cachefile
     Cache.read(read_file)
-    
-    
-    
+
     # If no output file is specified, let the conversion overwrite the input file
     if write_file is None:
         write_file = read_file
     else:
         copyfile(read_file, write_file)
-    
-    convert_cache_file(filestr=write_file,
-                       target_version=target)
 
+    convert_cache_file(filestr=write_file, target_version=target)
 
 
 def convert_t4(read_file: PathLike, write_file: PathLike):
     """The main function for handling the T4 conversion of a cachefile."""
     cache = read_cache(read_file)
 
-    t4_cache = convert_cache_to_t4(cache)
+    t4_cache: Any = convert_cache_to_t4(cache)
 
     write_cache(t4_cache, write_file)
-
 
 
 def merge(file_list: List[PathLike], outfile: PathLike):
@@ -215,7 +212,7 @@ def merge(file_list: List[PathLike], outfile: PathLike):
 
     First, we must validate the existence and validity of cachefiles, then we merge.
     """
-    if (len(file_list) < 2):
+    if len(file_list) < 2:
         raise ValueError("Not enough (< 2) files provided to merge.")
 
     # Perform validation, conversion, equivalence check and after merge.
@@ -232,4 +229,3 @@ def merge(file_list: List[PathLike], outfile: PathLike):
     merge_files(file_list, outfile)
 
     print(f"[*] Merging finished. Output file: '{outfile}'.")
-
