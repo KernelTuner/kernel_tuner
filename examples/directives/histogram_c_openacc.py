e7fd411d1db1d443be8084e81b8a11a03cb98c90
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2024-09-17 15:31:00 +0200

    Fixing a bug in the correctness check.

--- examples/directives/histogram_c_openacc.py
+++ examples/directives/histogram_c_openacc.py
@@ -4,13 +4,28 @@ import numpy as np
 
 from kernel_tuner import tune_kernel
 from kernel_tuner.utils.directives import Code, OpenACC, Cxx, process_directives
+from kernel_tuner.observers import BenchmarkObserver
 
 
+# Naive Python histogram implementation
 def histogram(vector, hist):
     for i in range(0, len(vector)):
-        item = vector[i]
-        hist[item] = hist[item] + 1
+        hist[vector[i]] += 1
     return hist
+
+
+# We use this observer to clean output memory in between kernel executions
+class MemoryReset(BenchmarkObserver):
+    def __init__(self, args):
+        self.args = args
+
+    def before_start(self):
+        for i, arg in enumerate(self.args):
+            if not arg is None:
+                self.dev.memcpy_htod(self.dev.allocations[i], arg)
+
+    def get_results(self):
+        return {}
 
 
 code = """
@@ -24,10 +39,11 @@ int main(void) {
 	int * hist = (int *) malloc(HIST_SIZE * sizeof(int));
 
 	#pragma tuner start histogram vector(int*:VECTOR_SIZE) hist(int*:HIST_SIZE)
-	#pragma acc parallel num_gangs(ngangs) vector_length(nthreads) reduction(+:hist)
+	#pragma acc parallel num_gangs(ngangs) vector_length(nthreads)
+	#pragma acc loop independent
 	for ( int i = 0; i < VECTOR_SIZE; i++ ) {
-	    int item = vector[i];
-		hist[item] = hist[item] + 1;
+	    #pragma acc atomic update
+		hist[vector[i]] += 1;
 	}
 	#pragma tuner stop
 
@@ -50,7 +66,7 @@ metrics["GB/s"] = (
     / 10**9
 )
 
-kernel_args["histogram"][0] = np.random.randint(0, 255, len(kernel_args["histogram"][0]), dtype=np.int32)
+kernel_args["histogram"][0] = np.random.randint(0, 256, len(kernel_args["histogram"][0]), dtype=np.int32)
 kernel_args["histogram"][1] = np.zeros(len(kernel_args["histogram"][1])).astype(np.int32)
 reference_hist = np.zeros_like(kernel_args["histogram"][1]).astype(np.int32)
 reference_hist = histogram(kernel_args["histogram"][0], reference_hist)
