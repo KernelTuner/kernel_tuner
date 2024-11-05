eba03f83689c70ed3a886cbdaa43a60acaabcfe7
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2024-11-05 15:46:12 -0800

    Refactored BO BOTorch into class structure

--- kernel_tuner/strategies/bayes_opt_BOTorch.py
+++ kernel_tuner/strategies/bayes_opt_BOTorch.py
@@ -1,13 +1,18 @@
 """Bayesian Optimization implementation using BO Torch."""
 
 import numpy as np
-import torch
-from botorch import fit_gpytorch_model
-from botorch.acquisition import ExpectedImprovement
-from botorch.models import SingleTaskGP
-from botorch.optim import optimize_acqf_discrete
-from gpytorch.mlls import ExactMarginalLogLikelihood
-from torch import Tensor
+
+try:
+    import torch
+    from botorch import fit_gpytorch_model
+    from botorch.acquisition import ExpectedImprovement
+    from botorch.models import SingleTaskGP
+    from botorch.optim import optimize_acqf_discrete
+    from gpytorch.mlls import ExactMarginalLogLikelihood
+    from torch import Tensor
+    bayes_opt_present = True
+except ImportError:
+    bayes_opt_present = False
 
 from kernel_tuner import util
 from kernel_tuner.searchspace import Searchspace
@@ -18,59 +23,78 @@ from kernel_tuner.strategies.common import (
 
 def tune(searchspace: Searchspace, runner, tuning_options):
     max_fevals = tuning_options.strategy_options.get("max_fevals", 100)
-    initial_sample_size = tuning_options.strategy_options.get("popsize", 20)
-    cost_func = CostFunc(searchspace, tuning_options, runner, scaling=False)
+    bo = BayesianOptimization(searchspace, runner, tuning_options)
+    return bo.run(max_fevals)
 
-    # function to optimize
-    def evaluate_function(X):
-        if isinstance(X, (Tensor, list)):
+class BayesianOptimization():
+    """Bayesian Optimization class."""
+
+    def __init__(self, searchspace: Searchspace, runner, tuning_options):
+        self.initial_sample_taken = False
+        self.initial_sample_size = tuning_options.strategy_options.get("popsize", 20)
+        self.tuning_options = tuning_options
+        self.cost_func = CostFunc(searchspace, tuning_options, runner, scaling=False)
+
+        # set up conversion to tensors
+        self.searchspace = searchspace
+        self.searchspace_tensors = torch.from_numpy(searchspace.get_list_numpy().astype(float))
+        self.train_X = torch.empty_like(self.searchspace_tensors)
+        self.train_Y = torch.empty(len(self.train_X))
+
+        # get bounds
+        bounds = []
+        for v in searchspace.params_values:
+            bounds.append([min(v), max(v)])
+        bounds = torch.from_numpy(np.array(bounds).transpose())
+
+    def evaluate_configs(self, X: Tensor):
+        """Evaluate a tensor of one or multiple configurations."""
+        if isinstance(X, Tensor):
             results = []
             if X.dim() == 1:
-                results = [[cost_func(X)]]
+                results = [[self.cost_func(X)]]
             else:
-                results = [[cost_func(c)] for c in X]
+                results = [[self.cost_func(c)] for c in X]
             return torch.from_numpy(np.array(results))
         else:
             raise NotImplementedError(f"Evaluation has not been implemented for type {type(X)}")
+        
+    def initial_sample(self):
+        """Take an initial sample."""
+        sample_indices = torch.from_numpy(self.searchspace.get_random_sample_indices(self.initial_sample_size))
+        self.train_X = self.searchspace_tensors.index_select(0, sample_indices)
+        self.train_Y = self.evaluate_configs(self.train_X)
+        self.initial_sample_taken = True
 
-    # set up conversion to tensors
-    full_space = torch.from_numpy(searchspace.get_list_numpy().astype(float))
+    def run(self, max_fevals: int):
+        """Run the Bayesian Optimization loop for at most `max_fevals`."""
+        try:
+            if not self.initial_sample_taken:
+                self.initial_sample()
 
-    # get bounds
-    bounds = []
-    for v in searchspace.params_values:
-        bounds.append([min(v), max(v)])
-    bounds = torch.from_numpy(np.array(bounds).transpose())
+            # Bayesian optimization loop
+            for _ in range(max_fevals):
+                # Fit a Gaussian Process model
+                gp = SingleTaskGP(self.train_X, self.train_Y)
+                mll = ExactMarginalLogLikelihood(gp.likelihood, gp)
+                fit_gpytorch_model(mll)
+                
+                # Define the acquisition function
+                ei = ExpectedImprovement(model=gp, best_f=self.train_Y.min(), maximize=False)
+                
+                # Optimize acquisition function to find the next evaluation point
+                candidate, _ = optimize_acqf_discrete(
+                    ei, 
+                    q=1, 
+                    choices=self.searchspace_tensors
+                )
+                
+                # Evaluate the new candidate and update the dataset
+                new_y = self.evaluate_configs(candidate)
+                self.train_X = torch.cat([self.train_X, candidate])
+                self.train_Y = torch.cat([self.train_Y, new_y])
+        except util.StopCriterionReached as e:
+            if self.tuning_options.verbose:
+                print(e)
 
-    try:
-        # take initial sample
-        sample_indices = torch.from_numpy(searchspace.get_random_sample_indices(initial_sample_size))
-        train_X = full_space.index_select(0, sample_indices)
-        train_Y = evaluate_function(train_X)
-
-        # Bayesian optimization loop
-        for _ in range(max_fevals):
-            # Fit a Gaussian Process model
-            gp = SingleTaskGP(train_X, train_Y)
-            mll = ExactMarginalLogLikelihood(gp.likelihood, gp)
-            fit_gpytorch_model(mll)
-            
-            # Define the acquisition function
-            ei = ExpectedImprovement(model=gp, best_f=train_Y.min(), maximize=False)
-            
-            # Optimize acquisition function to find the next evaluation point
-            candidate, _ = optimize_acqf_discrete(
-                ei, 
-                q=1, 
-                choices=full_space
-            )
-            
-            # Evaluate the new candidate and update the dataset
-            new_y = evaluate_function(candidate)
-            train_X = torch.cat([train_X, candidate])
-            train_Y = torch.cat([train_Y, new_y])
-    except util.StopCriterionReached as e:
-        if tuning_options.verbose:
-            print(e)
-
-    return cost_func.results
+        return self.cost_func.results 
