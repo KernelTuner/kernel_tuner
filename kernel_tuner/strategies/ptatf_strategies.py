268bf679323f7d52476db35ebf7b23fb505a9fe8
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2025-06-17 16:06:13 +0200

    Initial version of using external package strategies

---
+++ kernel_tuner/strategies/ptatf_strategies.py
@@ -0,0 +1,43 @@
+"""Strategy that enables the use of pyATF strategies."""
+
+from kernel_tuner.searchspace import Searchspace
+from kernel_tuner.strategies import common
+from kernel_tuner.strategies.common import CostFunc, setup_method_arguments, setup_method_options
+from kernel_tuner.util import StopCriterionReached
+
+supported_methods = ["Nelder-Mead", "Powell", "CG", "BFGS", "L-BFGS-B", "TNC", "COBYLA", "SLSQP"]
+
+_options = dict(method=(f"Local optimization algorithm to use, choose any from {supported_methods}", "L-BFGS-B"),
+                       T=("Temperature parameter for the accept or reject criterion", 1.0))
+
+def tune(searchspace: Searchspace, runner, tuning_options):
+    method, T = common.get_options(tuning_options.strategy_options, _options)
+
+    # scale variables in x to make 'eps' relevant for multiple variables
+    cost_func = CostFunc(searchspace, tuning_options, runner, scaling=True)
+
+    bounds, x0, eps = cost_func.get_bounds_x0_eps()
+
+    kwargs = setup_method_arguments(method, bounds)
+    options = setup_method_options(method, tuning_options)
+    kwargs['options'] = options
+
+
+    minimizer_kwargs = dict(**kwargs)
+    minimizer_kwargs["method"] = method
+
+    opt_result = None
+    try:
+        opt_result = scipy.optimize.basinhopping(cost_func, x0, T=T, stepsize=eps,
+                                             minimizer_kwargs=minimizer_kwargs, disp=tuning_options.verbose)
+    except StopCriterionReached as e:
+        if tuning_options.verbose:
+            print(e)
+
+    if opt_result and tuning_options.verbose:
+        print(opt_result.message)
+
+    return cost_func.results
+
+
+tune.__doc__ = common.get_strategy_docstring("basin hopping", _options)
