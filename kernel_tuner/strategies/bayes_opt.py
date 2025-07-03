e2924e0d8620de7f0b75ddd761211130a34ae788
fjwillemsen <fjwillemsen@icloud.com>
2025-07-03 09:02:20 +0200

    Improvements for Numpy 2.0 compatibility

--- kernel_tuner/strategies/bayes_opt.py
+++ kernel_tuner/strategies/bayes_opt.py
@@ -229,7 +229,7 @@ class BayesianOptimization:
             self.worst_value = np.inf
             self.argopt = np.argmin
         elif opt_direction == "max":
-            self.worst_value = np.NINF
+            self.worst_value = -np.inf
             self.argopt = np.argmax
         else:
             raise ValueError("Invalid optimization direction '{}'".format(opt_direction))
