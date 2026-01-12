114bb0b09c8471527a47ad2843e5b63cec7856ca
fjwillemsen <fjwillemsen@icloud.com>
2026-01-12 18:58:54 +0100

    Minor improvement to parameter index optimization technique selection

--- kernel_tuner/searchspace.py
+++ kernel_tuner/searchspace.py
@@ -726,8 +726,7 @@ class Searchspace:
                 tune_params_to_index_lookup.append({ value: index for index, value in enumerate(param_values) })
                 tune_params_from_index_lookup.append({ index: value for index, value in enumerate(param_values) })
                 if (all_values_integer_nonnegative and 
-                    not all(isinstance(v, int) and v >= 0 for v in param_values) or
-                    max(param_values) >= 2**15
+                    not all(isinstance(v, int) and 0 < v < 2**15 for v in param_values)
                 ):
                     all_values_integer_nonnegative = False
 
