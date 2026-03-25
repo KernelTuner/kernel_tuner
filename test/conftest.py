75dc87051403a571c9579cc448c62f8bc191fe72
fjwillemsen <fjwillemsen@icloud.com>
2026-03-25 16:06:52 +0100

    Extended timeout to allow Garbage Collection to complete

--- test/conftest.py
+++ test/conftest.py
@@ -4,4 +4,4 @@ import pytest
 def pytest_collection_modifyitems(items):
     for item in items:
         if item.get_closest_marker('timeout') is None:
-            item.add_marker(pytest.mark.timeout(60))
+            item.add_marker(pytest.mark.timeout(180))
