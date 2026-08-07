d171e13248cf4aecf244c93bca71c0bd43d38053
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2026-08-07 14:15:46 +0200

    Fix Julia undue argument list warning

--- kernel_tuner/util.py
+++ kernel_tuner/util.py
@@ -178,6 +178,10 @@ def check_argument_list(kernel_name, kernel_string, args, lang=None):
 
     # Check each set of kernel arguments
     for arguments_set, arguments in enumerate(kernel_arguments):
+
+        # check arguments and signature lengths
+        if lang.upper() == "JULIA" and len(arguments) > len(args):
+            continue    # for Julia tunable parameters are added to the kernel signature 
         collected_errors.append(list())
         if len(arguments) != len(args):
             collected_errors[arguments_set].append(
